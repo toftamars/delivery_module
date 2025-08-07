004fa9394ecdea1a97f6ee4f828162e9c6259a3b
toftamars <GitHub E-posta Adresiniz>
2025-08-07 23:39:35 +0300

    Manuel kurulum wizard'ı kaldırıldı

--- delivery_module/wizard/__init__.py
+++ delivery_module/wizard/__init__.py
@@ -4,5 +4,4 @@ from . import delivery_day_closure_wizard
 from . import delivery_vehicle_closure_wizard
 from . import setup_delivery_schedule_wizard
 from . import delivery_photo_wizard
-from . import cancel_confirmation_wizard
-from . import manual_setup_wizard 
+from . import cancel_confirmation_wizard 
