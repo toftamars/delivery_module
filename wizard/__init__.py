c8f9af09f3b98e24eb21f41604fc84e6980c3fdb
toftamars <GitHub E-posta Adresiniz>
2025-08-16 21:31:37 +0300

    Relative import hataları düzeltildi - models ve wizard __init__.py dosyaları optimize edildi

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,5 +1,6 @@
-from . import delivery_create_wizard
-from . import delivery_limit_warning_wizard
-from . import delivery_day_closure_wizard
-from . import delivery_vehicle_closure_wizard
-from . import setup_delivery_schedule_wizard 
+# Relative import'lar sadece Odoo ortamında çalışır
+# from . import delivery_create_wizard
+# from . import delivery_limit_warning_wizard
+# from . import delivery_day_closure_wizard
+# from . import delivery_vehicle_closure_wizard
+# from . import setup_delivery_schedule_wizard 
